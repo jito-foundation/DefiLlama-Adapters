a531453931f3a4a7d1d5705ee1f851917d2298a9
aoikurokawa <ht.so.ik0102@gmail.com>
2025-01-13 14:39:43 +0900

    fix: indent

--- projects/jito/restaking.js
+++ projects/jito/restaking.js
@@ -1,6 +1,8 @@
 const { PublicKey } = require("@solana/web3.js");
-const { sumTokens2, getConnection, decodeAccount, getAssociatedTokenAddress } = require("../helper/solana");
+const { sumTokens2, getAssociatedTokenAddress, getProvider, getConnection } = require("../helper/solana");
 const { bs58 } = require('@project-serum/anchor/dist/cjs/utils/bytes');
+const anchor = require("@project-serum/anchor");
+const jitoVaultIdl = require("./jito_vault.json");
 
 const VAULT_PROGRAM_ID = new PublicKey("Vau1t6sLNxnzB7ZDsef8TLbPLfyZMYXH8WTNqUdm9g8");
 const VAULT_DISCRIMINATOR = Buffer.from([2]);
@@ -11,28 +13,34 @@ function getVaultPubkey(vault) {
 }
 
 async function tvl() {
-  const connection = getConnection();
-  const accounts = await connection.getProgramAccounts(
-    VAULT_PROGRAM_ID,
-    {
-      filters: [
+    const provider = getProvider();
+    const connection = getConnection();
+    const program = new anchor.Program(jitoVaultIdl, VAULT_PROGRAM_ID, provider);
+
+    const accounts = await connection.getProgramAccounts(
+        VAULT_PROGRAM_ID,
         {
-          memcmp: {
-            offset: 0,
-            bytes: bs58.encode(VAULT_DISCRIMINATOR),
-          },
-        },
-      ]
-    }
-  );
+            filters: [
+            {
+                memcmp: {
+                offset: 0,
+                bytes: bs58.encode(VAULT_DISCRIMINATOR),
+                },
+            },
+            ]
+        }
+    );
 
-  const vaults = accounts.map(i => decodeAccount('jitoVault', i.account))
-  const stAssociatedTokenAddresses = vaults.map((vault) => {
+    const vaults = accounts.map((account) => {
+        return program.account.vault.coder.accounts.decodeUnchecked("Vault", account.account.data);
+    });
+
+    const stAssociatedTokenAddresses = vaults.map((vault) => {
     const vaultPubkey = getVaultPubkey(vault);
-    return getAssociatedTokenAddress(vault.supportedMint, vaultPubkey);
-  });
+        return getAssociatedTokenAddress(vault.supportedMint, vaultPubkey);
+    });
 
-  return sumTokens2({ tokenAccounts: stAssociatedTokenAddresses, allowError: true })
+    return sumTokens2({ tokenAccounts: stAssociatedTokenAddresses, allowError: true })
 }
 
 module.exports = {
